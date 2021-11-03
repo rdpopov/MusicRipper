d050ad33686f8365a5d2eee591fbba8ac795b257
Rosen Popov <rdonkow@gmail.com>
2021-11-03 02:29:25 +0200

    more changes so now it works ish

--- README.md
+++ README.md
@@ -2,13 +2,14 @@
 MusicRipper is an instrument based on youtube-dl, to download music.
 Terminal based. 
 The whole idea is to be able to have something to rip out music from youtube.
-Using just a playlist url
+Using just a playlist url.
+
 
 # Features(TODO)
-    #### Minimal update
-        - Updates are very slow, there should be a way to do that more
-            efficiently
-        - editing outout file metadata to be a little bit more automatic.
-        - Possibly add lyrics and artwork from somewhere(maybe thumbnail)
-        - Will add more
-
+#### Minimal update
+  - Updates are very slow, there should be a way to do that more
+      efficiently
+  - editing output file metadata to be a little bit more automatic.
+    - Automatic adding the name and artist and other metadata
+  - Possibly add lyrics and artwork from somewhere(maybe thumbnail)
+  - Will add more
--- grabber.py
+++ grabber.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 
 import youtube_dl
+import os
+import re
 # import mutagen # to edit tags of file
 
 
@@ -10,7 +12,8 @@ def print_key_if(col, key):
     else:
         print("no key", key)
 
-def to_pth(pth,suf):
+
+def to_pth(pth, suf):
     if pth[-1] == '/':
         return pth + suf
     return pth + '/' + suf
@@ -18,65 +21,83 @@ def to_pth(pth,suf):
 
 def if_key(col, key):
     if key in col:
-        return col[key]
+        return col[key].replace(' ','_')
     else:
         return None
 
+
 global_settings = {
-        '--force_replace': True,
+        '--force-replace': True,
         '--no_structure': False,
+        '--path': os.getcwd(),
+        '--playlists-csv': 'library.csv',
+        '--output-folder': 'Music'
 }
 
 
-class Song:
-    def __init__(self, vid_info):
-        self._artist = if_key(i, 'artist')
-        self._album = if_key(i, 'album')
-        self._title = if_key(i, 'title')
-        self._alt_title = if_key(i, 'alt_title')
-        self._webpage_url = if_key(i, 'webpage_url')
+class song:
+    def __init__(self, vid_info,sub_path = None):
+        self._artist     = if_key(vid_info, 'artist')
+        self._album      = if_key(vid_info, 'album')
+        self._title      = if_key(vid_info, 'title')
+        self._alt_title  = if_key(vid_info, 'alt_title')
+        self._webpage_url= if_key(vid_info, 'webpage_url')
+        self.name = self.form_name()
+        self.path = self.form_path(sub_path)
 
-    def form_name(self,path):
-        fmt_map = {}
-        if not self._alt_title or not self._artist:
-            fmt_map['name'] = self._title.replace(' ', '_')
-        fmt_map['name'] = "{}-{}.mp3".format(self._artist, self._alt_title)
-        if not self._album or not self._artist:
-            fmt_map['local_path'] = 'Unkown'
-        fmt_map['path']
+    def form_name(self):
+        if self._alt_title and self._artist:
+            return "{}-{}.mp3".format(self._artist, self._alt_title)
+        else:
+            return re.sub("[(].*[)]", '', self._title.replace(' ', '_'))
 
-    def download(self,root_path,artist_and_album,force=False):
-        ydl_opts = {
-                'format': 'bestaudio/best',
-                'postprocessors': [{
-                    'key': 'FFmpegExtractAudio',
-                    'preferredcodec': 'mp3',
-                    'preferredquality': '192',
-                    }],
-                }
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            ydl.download(self._webpage_url)
+    def form_path(self, sub_path):
+        fname = self.form_name()
+        alb = self._album or 'Unknown'
+        art = self._artist or 'Unknown'
+        whr = art + '/' + alb
+        whr = to_pth(whr, fname)
+        pth = to_pth(global_settings['--path'],
+                     global_settings['--output-folder'] + '/' + sub_path)
+        if global_settings['--no_structure']:
+            return to_pth(pth, fname)
+        return to_pth(pth, whr)
 
+    def fix_metadata():
+        print("not yet implemented")
+        pass
+
+    def download(self):
+        if not (os.path.isfile(self.path) or os.path.isfile(self.name)) or global_settings['--force_replace']:
+            ydl_opts = {
+                        'format': 'bestaudio',
+                        'postprocessors': [{
+                            'key': 'FFmpegExtractAudio',
+                            'preferredcodec': 'mp3',
+                            'preferredquality': '192'
+                            }],
+                        'prefer_ffmpeg': True,
+                        'keepvideo': False,
+                        'outtmpl': self.path,
+                    }
+            with youtube_dl.YoutubeDL(ydl_opts) as down:
+                down.extract_info(self._webpage_url,download=True)
 
 
 if __name__ == '__main__':
-    
-    ydl = youtube_dl.YoutubeDL({'outtmpl': '%(id)s.%(ext)s'})
-    with ydl:
-        result = ydl.extract_info(
-            'https://www.youtube.com/watch?v=eJnQBXmZ7Ek&list=PLX1RXtit_D_2E17WA0uqyvhi0CJcfkJRC',
-            download=False
-        )
-    if 'entries' in result:
-        video = result['entries']
-    else:
-        video = result
-    print(video[0].keys())
-    
-    for i in video:
-        print('--------------------------------------------------------------------')
-        print_key_if(i,'artist')
-        print_key_if(i,'album')
-        print_key_if(i,'title')
-        print_key_if(i,'alt_title')
-        print(i['title'],i['webpage_url'])
+    with open(global_settings['--playlists-csv'], 'r') as playlists:
+        ydl = youtube_dl.YoutubeDL()
+        for i in playlists:
+            name, link= i.split(',')
+            with ydl:
+                result = ydl.extract_info(
+                    link,
+                    download=False)
+                if 'entries' in result:
+                    video = result['entries']
+                else:
+                    video = result
+                print(video[0].keys())
+                for i in video:
+                    trc = song(i,name)
+                    trc.download()
