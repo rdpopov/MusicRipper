10bd216f77ebde5431b93fa5f3346b711362d2ca
Rosen Popov <rdonkow@gmail.com>
2022-09-23 13:05:52 +0300

    Finished test with mocking input

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 init:
-	pip install -r requirements.txt
+	pip install -r requirements.txt || pip3 install -r requirements.txt
 test:
-	python -m unittest
+	python -m unittest || python3 -m unittest
 # add server
 # add normal operation
 
--- test/test_cmd_line_util.py
+++ test/test_cmd_line_util.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python
 import unittest
+from unittest.mock import patch
 import sys
 import os
+from io import StringIO
+
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 from cmd_line_util import GenericInput
 
+mock_input = StringIO(initial_value='1\n2\n3\n4\n5\n6\n7\n8\n9\n10')
+
 class TestInputWrapper(unittest.TestCase):
     def setUp(self):
-        self.test_contents = [str(a) for a in range(10)]
-        self.test_input_file = "test_input"
+        self.test_contents = [str(a) for a in range(1,11)]
+        self.test_input_file = "./test_input"
         with open(self.test_input_file, "w") as f:
             for i in self.test_contents:
                 f.write(i+"\n")
@@ -16,19 +21,33 @@ class TestInputWrapper(unittest.TestCase):
     def test_file_input(self):
         inputter = GenericInput([self.test_input_file], False)
         for contr, test in zip(self.test_contents, inputter.get_input()):
-            # print(contr, test)
             self.assertEqual(contr, test, "Read incorrect value contr{} != test{}\n".format(contr, test))
 
     def test_multiple_file_input(self):
-        pass
+        inputter = GenericInput([self.test_input_file, self.test_input_file], False)
+        for contr, test in zip(self.test_contents * 2, inputter.get_input()):
+            self.assertEqual(contr, test, "Read incorrect value contr{} != test{}\n".format(contr, test))
 
-    def test_multiple_file_input_stdin(self):
-        pass
-
+    @patch('sys.stdin', mock_input)
     def test_stdin(self):
-        pass
+        inputter = GenericInput([], True)
+        for contr, test in zip(self.test_contents, inputter.get_input()):
+            self.assertEqual(contr, test, "Read incorrect value contr{} != test{}\n".format(contr, test))
 
-    def tear_stdin_input_stdin_false(self):
+    @patch('sys.stdin', mock_input)
+    def test_multiple_file_input_stdin(self):
+        inputter = GenericInput([self.test_input_file, self.test_input_file], True)
+        for contr, test in zip(self.test_contents * 3, inputter.get_input()):
+            self.assertEqual(contr, test, "Read incorrect value contr{} != test{}\n".format(contr, test))
+
+    @patch('sys.stdin', mock_input)
+    def test_stdin_input_stdin_false(self):
+        inputter = GenericInput([self.test_input_file, self.test_input_file], False)
+        for contr, test in zip(self.test_contents * 2, inputter.get_input()):
+            self.assertEqual(contr, test, "Read incorrect value contr{} != test{}\n".format(contr, test))
+
+    def tearDown(self):
+        os.remove(self.test_input_file)
         pass
 
 if __name__ == '__main__':
